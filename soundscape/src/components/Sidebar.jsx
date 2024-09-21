e2f07630727bf0105f875f5a1bffef6837407f55
github <kumudsalunke26@gmail.com>
2024-09-21 12:34:31 +0530

    sidebar modifications

--- soundscape/src/components/Sidebar.jsx
+++ soundscape/src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import {assets} from '../assets/assets'
 const Sidebar = () => {
   return (
     <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
-      <div className='bg-[#121212] h-[15%] rounded flex flex-col justify-around'>
+      <div className='bg-[#151515] h-[15%] rounded flex flex-col justify-around'>
         <div className='flex items-center gap-3 pl-8 cursor-pointer'>
            <img className='w-6' src={assets.home_icon} />
            <p className='font-bold'>Home</p>

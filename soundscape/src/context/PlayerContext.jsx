a6f249d9b0c28308b34ed5ab4f65827d3df2350e
github <kumudsalunke26@gmail.com>
2024-10-22 00:21:56 +0530

    songs functionality

---
+++ soundscape/src/context/PlayerContext.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useRef, useState } from "react";
+import { createContext } from "react";
+import { songsData } from "../assets/assets";
+
+export const PlayerContext = createContext();
+const PlayerContextProvider = (props) => {
+
+    const audioRef = useRef();
+    const seekBar = useRef();
+    const seekBg = useRef();
+
+    const [track, setTrack] = useState(songsData[2]);
+    const [playStatus, setPlayStatus] = useState(false);
+    const [time, setTime] = useState({
+        currentTime: {
+            second: 0,
+            minute: 0
+        },
+        totalTime: {
+            second: 0,
+            minute: 0
+        }
+    });
+    const play = () => {
+        audioRef.current.play();
+        setPlayStatus(true);
+    }
+
+    const pause = () => {
+        audioRef.current.pause();
+        setPlayStatus(false);
+    }
+
+useEffect(()=>{
+    setTimeout(()=>{
+        audioRef.current.ontimeupdate=()=>{
+            seekBar.current.style.width=(Math.floor(audioRef.current.currentTime/audioRef.current.duration*100))+"%";
+        setTime({
+            currentTime: {
+                second: Math.floor(audioRef.current.currentTime%60),
+                minute: Math.floor(audioRef.current.currentTime/60)
+            },
+            totalTime: {
+                second: Math.floor(audioRef.current.duration%60),
+                minute: Math.floor(audioRef.current.duration/60)
+            }
+        })
+        }
+    })
+
+},[audioRef])
+    const contextValue = {
+        audioRef,
+        seekBar,
+        seekBg,
+        track,setTrack,
+        playStatus,setPlayStatus,
+        time,setTime,
+        play,
+        pause
+    }
+    return (
+        <PlayerContext.Provider value={contextValue}>
+            {props.children}
+        </PlayerContext.Provider>
+    )
+}
+
+export default PlayerContextProvider;

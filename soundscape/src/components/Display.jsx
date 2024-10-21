b5080bf2efe7f330988f9f4f2cb15c812ce7826d
github <kumudsalunke26@gmail.com>
2024-10-21 20:46:53 +0530

    bgcolor

--- soundscape/src/components/Display.jsx
+++ soundscape/src/components/Display.jsx
@@ -1,12 +1,26 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import DisplayHome from './DisplayHome'
 import DisplayAlbum from './DisplayAlbum'
+import { albumsData } from '../assets/assets'
 
 
 const Display = () => {
   const displayRef = useRef();
-  const location= useLocation();
+  const location=useLocation();
+  const isAlbum=location.pathname.includes("album");
+  const albumId=isAlbum? location.pathname.slice(-1):"";
+  const bgColor= albumsData[Number(albumId)].bgColor;
+
+  useEffect(()=>{
+    if(isAlbum){
+    displayRef.current.style.background=`linear-gradient(${bgColor},#121212)`
+    }
+    else{
+       displayRef.current.style.background=`#121212`
+    }
+  }
+  )
   return (
     <div ref={displayRef} className='w-[100%] m-2 px-6 py-4 rounded bg-black text-white overflow-auto lg:w-[75%] lg:ml-0'>
       <Routes>
